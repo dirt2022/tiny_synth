fef478ef36400d5e012e066e35068681c68e2b63
dirt2022 <fk186@qq.com>
2026-02-26 19:22:40 +0800

    Add: Support for big-endian platform.

--- backend.c
+++ backend.c
@@ -2,12 +2,36 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/endian.h>
 #include <pulse/simple.h>
 #include <sys/types.h>
 #include "include/player_backend.h"
 #include "include/player_limit.h"
 
+#define SWAP32BIT(x) \
+	x=( ((unsigned int)x)<<16)|( ((unsigned int)x)>>16)
+
 // Note: All backends uses signed float32 (little endian). which is not limit.
+static union {
+	unsigned int a;
+	unsigned char b;
+} be_check;
+
+static int is_be(void){
+	be_check.a=1;
+	if (be_check.b == 0){
+		return 1;
+	}
+	return 0;
+}
+
+static int is_be_cpu;
+
+static void buffer_be2le(float * buffer,int size){ // used only on be platform
+	for(unsigned int i=0;i<size/sizeof(float);i++){
+		SWAP32BIT(buffer[i]);
+	}
+}
 
 static struct pa_simple * PulseAudio_Init(){ // the only backend.
 	pa_sample_spec spec;
@@ -19,10 +43,16 @@ static struct pa_simple * PulseAudio_Init(){ // the only backend.
 }
 
 int PulseAudio_Write(backend_stream_t s,void * buffer,size_t size){
+	if (is_be_cpu==1){
+		buffer_be2le(buffer,size);
+	}
 	return pa_simple_write((struct pa_simple *)s,buffer,size,NULL);
 }
 
 int File_Write(backend_stream_t s,void * buffer,size_t size){
+	if (is_be_cpu==1){
+		buffer_be2le(buffer,size);
+	}
 	int res=0;
 	for(size_t i=0;i < size;i++){
 		res=fputc(((char *)buffer)[i],s);
@@ -39,6 +69,7 @@ int(* BackendWrite)(backend_stream_t s,void * buffer,size_t size)=NULL;
 backend_stream_t backend_init(enum backend_type type){ //thread unsafe
 	static int inited=0;
 	backend_stream_t stream=NULL;
+	is_be_cpu=is_be();
 	if (inited == 1){
 		printf("Fatal: Backend should not be inited twice.\n");
 		abort();
