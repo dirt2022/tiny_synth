0fac3b534abda4dfed22c87b3005df6b0129eb46
dirt2022 <fk186@qq.com>
2026-08-03 23:58:35 +0800

    Math: use a negv table, to avoid fneg instruction.

--- math.c
+++ math.c
@@ -4,8 +4,10 @@
 #include "include/player_math.h"
 #include "include/player_limit.h"
 
-static float sintab[SINTAB_LEN];
+static float sintab[SINTAB_LEN+1];
+static float sintab_negv[SINTAB_LEN+1];
 
+// note sintab[SINTAB_LEN]=1.0f is just to prevent overflow. It doesn't belong to sintab.
 // only used by fastsin
 #define BACKEND_RATE_DIV_2 BACKEND_RATE/2
 #define BACKEND_RATE_DIV_4 BACKEND_RATE/4
@@ -14,6 +16,11 @@ void sintab_init(void){
 	for (unsigned int i=0;i < SINTAB_LEN;i++){
 		sintab[i]=sin((float)i/(float)SINTAB_LEN*PI_DIV_2);
 	}
+	sintab[SINTAB_LEN]=1.0f;
+	for (unsigned int i=0;i < SINTAB_LEN;i++){
+		sintab_negv[i]=-sintab[i];
+	}
+	sintab_negv[SINTAB_LEN]=-1.0f;
 return;
 }
 
@@ -29,9 +36,6 @@ float fastsin(int x){ // note 1*BACKEND_RATE is considered to be a cycle (2*pi)
 	if (x > BACKEND_RATE_DIV_4){
 		x=BACKEND_RATE_DIV_2-x;
 	}
-	if (x == BACKEND_RATE_DIV_4){
-		return flip ? -1.0f:1.0f;
-	}
 	index=(unsigned int)(((float)x)*((float)SINTAB_LEN)/((float)BACKEND_RATE_DIV_4));
-	return flip? -sintab[index]:sintab[index];
+	return flip? sintab_negv[index]:sintab[index];
 }
